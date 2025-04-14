23d5efd42e590251ec3cc4d13c1f5627c8bb915c
nurlybek <orozovnurlybek@gmail.com>
2025-04-14 13:27:33 +0600

    Настроен сервер Express, добавлен мултер для файлов

---
+++ index.ts
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import artistRouter from "./routers/artist";
+import albumRouter from "./routers/album";
+import trackRouter from "./routers/track";
+
+const app = express();
+const port = 8000;
+
+app.use(cors());
+app.use(express.static("public"));
+app.use(express.json());
+app.use('/artists', artistRouter)
+app.use('/albums', albumRouter)
+app.use('/tracks', trackRouter)
+
+
+const run = async () => {
+    await mongoose.connect(`mongodb://localhost/HW-82`);
+
+    app.listen(port, () => {
+        console.log(`Server started on http://localhost:${port}`);
+    })
+
+    process.on('exit', () => {
+        mongoose.disconnect();
+    })
+}
+run().catch(console.error);
