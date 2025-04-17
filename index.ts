57f0f4d4f7bb2ed3dd3f172a53f0441bff41ca61
nurlybek <orozovnurlybek@gmail.com>
2025-04-17 23:00:36 +0600

    [HW-83] Добавлены роутеры c пост запросами для users и track_history

--- index.ts
+++ index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import artistRouter from "./routers/artist";
 import albumRouter from "./routers/album";
 import trackRouter from "./routers/track";
+import usersRouter from "./routers/users";
+import TrackHistory from "./routers/trackHistory";
 
 const app = express();
 const port = 8000;
@@ -14,6 +16,8 @@ app.use(express.json());
 app.use('/artists', artistRouter)
 app.use('/albums', albumRouter)
 app.use('/tracks', trackRouter)
+app.use('/users', usersRouter)
+app.use('/track_history', TrackHistory)
 
 
 const run = async () => {
